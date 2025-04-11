fd0fd4dc9ba98bf75f334bc6c80916522bd9105f
Mohsen <mohsen.z4985@gmial.com>
2025-04-11 14:00:01 +0330

    complete Menus

---
+++ src/model/Enums/commands/GameCommands.java
@@ -0,0 +1,30 @@
+package model.Enums;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public enum GameCommands implements Commands {
+    exitGame(""),
+    nextTurn(""),
+    showTime(""),
+    showDate(""),
+    showDateTime(""),
+    showDayOfWeek(""),
+    cheatAdvancedTime(""),
+    cheatAdvancedDate(""),
+
+    private final String pattern;
+
+    GameCommands(String pattern) {
+        this.pattern = pattern;
+    }
+
+    public boolean isValid(String input) {
+        return java.util.regex.Pattern.matches(this.pattern, input);
+    }
+
+    public Matcher getMatcher(String input) {
+        Pattern compiledPattern = java.util.regex.Pattern.compile(this.pattern);
+        return compiledPattern.matcher(input);
+    }
+}
