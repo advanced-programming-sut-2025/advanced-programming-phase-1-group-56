dd168c5b878f770274f66457eecd9ce06b492553
Nima <Nazary1444@gmail.com>
2025-04-14 00:21:51 +0330

    add Artesian Recepie

--- src/model/items/CraftingTool.java
+++ src/model/items/CraftingTool.java
@@ -1,13 +1,18 @@
 package model.items;
 
+import model.Activities.ArtesianRecepie;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class CraftingTool extends Item {
-    private String name;
-    private String description;
-    private String sellPrice;
+    private final String name;
+    private final String description;
+    private final String sellPrice;
+    private ArrayList<ArtesianRecepie> craftingRecepie = new ArrayList<>();
 
     public CraftingTool(String name, String description, HashMap<Item, Integer> ingredients, String sellPrice) {
+        super(name,1,false);
         this.name = name;
         this.description = description;
         this.sellPrice = sellPrice;
@@ -24,5 +29,13 @@ public class CraftingTool extends Item {
 
     public String getSellPrice() {
         return sellPrice;
+    }
+
+    public ArrayList<ArtesianRecepie> getCraftingRecepie() {
+        return craftingRecepie;
+    }
+
+    public void setCraftingRecepie(ArrayList<ArtesianRecepie> craftingRecepie) {
+        this.craftingRecepie = craftingRecepie;
     }
 }
