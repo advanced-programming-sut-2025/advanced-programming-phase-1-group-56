6f460cbe6971075215ad08448713be8cfae56af8
Mohsen <mohsen.z4985@gmial.com>
2025-04-28 12:13:44 +0330

    delete something

--- gradleSetup/src/main/java/view/ProfileMenu.java
+++ gradleSetup/src/main/java/view/ProfileMenu.java
@@ -3,7 +3,7 @@ package view;
 import controller.MenuController.ProfileMenuController;
 import model.App;
 import model.Enums.Menu;
-import model.Enums.commands.ProfileMenuRegexes;
+import model.Enums.commands.ProfileMenuCommands;
 
 import java.util.Scanner;
 import java.util.regex.Matcher;
@@ -13,19 +13,19 @@ public class ProfileMenu implements AppMenu {
     public void check(Scanner scanner) {
         String input = scanner.nextLine();
         Matcher matcher;
-        if(ProfileMenuRegexes.ShowCurrentMenu.getMatcher(input) != null) {
+        if(ProfileMenuCommands.ShowCurrentMenu.getMatcher(input) != null) {
             System.out.println("you are in profile menu");
-        } else if((matcher = ProfileMenuRegexes.changeEmail.getMatcher(input)) != null) {
+        } else if((matcher = ProfileMenuCommands.changeEmail.getMatcher(input)) != null) {
             System.out.println(ProfileMenuController.manageChangeEmail(matcher.group(1)).getMessage());
-        } else if((matcher = ProfileMenuRegexes.changePassword.getMatcher(input)) != null) {
+        } else if((matcher = ProfileMenuCommands.changePassword.getMatcher(input)) != null) {
             System.out.println(ProfileMenuController.manageChangePassword(matcher.group(1),matcher.group(2)).getMessage());
-        } else if((matcher = ProfileMenuRegexes.changeUserName.getMatcher(input)) != null) {
+        } else if((matcher = ProfileMenuCommands.changeUserName.getMatcher(input)) != null) {
             System.out.println(ProfileMenuController.manageChangeUsername(matcher.group(1)).getMessage());
-        } else if(ProfileMenuRegexes.changeNickName.getMatcher(input) != null) {
+        } else if(ProfileMenuCommands.changeNickName.getMatcher(input) != null) {
             System.out.println(ProfileMenuController.manageChangeNickName(matcher.group(1)).getMessage());
-        } else if(ProfileMenuRegexes.showUserInformation.getMatcher(input) != null) {
+        } else if(ProfileMenuCommands.showUserInformation.getMatcher(input) != null) {
             System.out.println(ProfileMenuController.UserInfo().getMessage());
-        } if (ProfileMenuRegexes.BackToMainMenu.getMatcher(input) != null) {
+        } if (ProfileMenuCommands.BackToMainMenu.getMatcher(input) != null) {
             System.out.println("Bezan Berim Be Main Menu...");
             App.setCurrentMenu(Menu.mainMenu);
         }else {
