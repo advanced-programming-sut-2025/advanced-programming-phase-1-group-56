77664d996d03a3b2e1f2e1f1daf23fdb0efdc336
Mohsen <mohsen.z4985@gmial.com>
2025-05-14 02:43:14 +0330

    add shop menu to view

---
+++ gradleSetup/src/main/java/view/GameMenus/ShopMenus/MarniesRanchMenu.java
@@ -0,0 +1,12 @@
+package view.GameMenus.ShopMenus;
+
+import view.AppMenu;
+
+import java.util.Scanner;
+
+public class MarniesRanchMenu implements AppMenu {
+    @Override
+    public void check(Scanner scanner) {
+
+    }
+}
