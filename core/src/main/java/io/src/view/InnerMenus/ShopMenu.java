0774fc36fa6826143e75b96c76530ef34e3c7cee
Notebook <ar1umak1an@gmail.com>
2025-08-08 19:47:13 +0330

    v0.5.0: shop menu

--- core/src/main/java/io/src/view/InnerMenus/ShopMenu.java
+++ core/src/main/java/io/src/view/InnerMenus/ShopMenu.java
@@ -1,10 +1,21 @@
 package io.src.view.InnerMenus;
 
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Window;
+import io.src.model.GameObject.NPC.NpcProduct;
+
+import java.util.ArrayList;
 
 public class ShopMenu extends Window {
-    public ShopMenu(String title, Skin skin) {
-        super(title, skin);
+    private Image npcImage;
+    private ScrollPane productsScrollPane;
+    private ArrayList<NpcProduct> npcProducts;
+
+    public ShopMenu(Skin skin, String npcImageAssetName, ArrayList<NpcProduct> products ) {
+        super("", skin, "default4");
+
+        setMovable(false);
     }
 }
